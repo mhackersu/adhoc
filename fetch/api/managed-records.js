e14b0e5e532820b5c00ad04715bd5c31f878abfc
Mike Hacker <mhackersu@gmail.com>
2021-03-09 15:45:12 -0600

    tests passing

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -1,6 +1,7 @@
 import fetch from '../util/fetch-fill';
 import URI from 'urijs';
 
+// records endpoint
 window.path = 'http://localhost:3000/records';
 
 // Main Retrieve Function
@@ -10,108 +11,127 @@ const retrieve = (options) => {
     options = {};
   };
 
-  // Call queryStringHelper
-  let queryString = queryStringHelper(options, path).toString();
-  return fetch(query).then(response => {
-    let json = response.json();
-    if (response.ok) {
-      return json;
-    } else {
-      return json.then(Promise.reject.bind(Promise));
-    }
-  })
-  // Call getRecordsHelper
-  .then(response => getRecordsHelper(res, options))
-  .catch(e => console.log("Error: ", e))
+  let queryString = buildQueryString(options, path).toString();
+
+  return fetch(queryString)
+    .then(res => {
+      let json = res.json();
+      if (res.ok) {
+        return json;
+      } else {
+        return json.then(Promise.reject.bind(Promise));
+      }
+    })
+    // call returnRecordsHelper
+    .then(res => returnRecordsHelper(res, options))
+    .catch(e => console.log('error: ', e))
 };
 
-// Query String Helper
-const queryStringHelper = (options, url) => {
+// Set Options
+const buildQueryString = (options, url) => {
 
-  let colors = options["colors"] ? options["colors"] : ["red", "brown", "blue", "yellow", "green"];
-  let offset = options["page"] ? (options["page"] * 10) - 10 : 0;
-  let queryString = URI(url).addSearch("limit", 10).addSearch("offset", offset).addSearch("color[]", colors);
+  let colors = options['colors'] ? options['colors'] : ['red', 'brown', 'blue', 'yellow', 'green'];
+  let offset = options['page'] ? (options['page'] * 10) - 10 : 0;
+
+  let queryString = URI(url)
+    .addSearch('limit', 10)
+    .addSearch('offset', offset)
+    .addSearch('color[]', colors);
 
   return queryString;
 };
 
-// Return Records Helper
 const returnRecordsHelper = (resList, options) => {
-  
-  // Call getRecordsHelper and getPagesHelper
-  return getPagesHelper(resList, options).then(res => getRecordsHelper(res, resList, options)).catch(e => console.log("Error: ", e));
+
+  // call getPagesHelper
+  return getPagesHelper(resList, options)
+  // call getRecordsHelper
+  .then(res => getRecordsHelper(res, resList, options))
+  .catch(e => console.log('error: ', e))
 }
 
-// Get Records Helper
-const getRecordsHelper = (recordResults, resList, options) => {
+// Set Dispositions
+const getRecordsHelper = (pageResults, resList, options) => {
 
-  let ids = [];
-  let closed = 0;
-  let open = [];
-  let pgNum = options["page"] || 0;
-  let primaryColors = ["red", "blue", "yellow"];
+  let idsArray = [];
+  let closedCount = 0;
+  let openElements = [];
+  let pageNumber = options['page'] || 0;
+  const primaryColors = ['red', 'blue', 'yellow'];
 
   resList.forEach((element) => {
-    ids.push(element["id"]);
-    if (element["disposition"] === "open") {
-      element ["isPrimary"] = primaryColors.includes(element["color"]);
-      open.push(element);
-    } else if (element["disposition"] === "closed") {
-      if (primaryColors.includes(element["color"]) === true) {
-        closed++;
-      }
-    };
-  });
-  
-  let record = {
-    prevPage : recordResults["previous"],
-    nextPage : recordResults["next"],
-    itemId : ids,
-    openItem : open,
-    closedItem : closed
+
+    idsArray.push(element['id']);
+
+    if (element['disposition'] == 'open') {
+
+      element['isPrimary'] = primaryColors.includes(element['color']);
+      openElements.push(element);
+
+    } else if (element['disposition'] == 'closed') {
+
+      if (primaryColors.includes(element['color']) == true) {
+          closedCount++;
+        }
+      };
+    });
+
+  let recordObject = {
+    previousPage : pageResults['previous'],
+    nextPage: pageResults['next'],
+    ids : idsArray,
+    open : openElements,
+    closedPrimaryCount: closedCount
   };
 
-  return record;
+  return recordObject;
 };
 
-// Get Pages Helper
+// Pagination
 const getPagesHelper = (resList, options) => {
 
-  let colors = options["colors"];
-  let currentPage = options["page"] ? options["page"]: 1;
-  let previous_page;
-  let next_page;
-  let  = {
+  let colors = options['colors']
+  let currentPage = options['page'] ? options['page']: 1;
+  let callPreviousPage;
+  let callNextPage;
+  let pages = {
     previous: null,
     next: null
   };
 
-  if (currentPage === 1) {
-    previous_page = null;
-  // Call getPageNeighborsHelper
+  if (currentPage == 1) {
+    callPreviousPage = null;
   } else {
-    previous_page = getPageNeighborsHelper(path,{ page: currentPage - 1, colors: colors}).then(res => page["previous"] = res.length > 0 ? currentPage - 1 : null).catch(e => console.log("Error: ", e));
-  };
-
-  if (resList.length < 10) {
-    next_page = null
-  } else {
-    next_page = getPageNeighborsHelper(path, { page: currentPage + 1, colors: colors}).then(res => page["next"] = res.length > 0 ? currentPage + 1 : null).catch(e => console.log("Error: ", e));
+    // call getPageNeighborsHelper
+    callPreviousPage = getPageNeighborsHelper(path,{ page: currentPage - 1, colors: colors })
+      .then(res => pages['previous'] = res.length > 0 ? currentPage - 1 : null )
+      .catch(e => console.log('error: ', e))
   }
 
-  return Promise.all([previous_page, next_page]).then(res => page).catch(e => console.log("Error: ", e));
+  if (resList.length < 10) {
+    callNextPage = null
+  } else {
+    // call getPageNeighborsHelper
+    callNextPage = getPageNeighborsHelper(path, { page: currentPage + 1, colors: colors })
+      .then(res => pages['next'] = res.length > 0 ? currentPage + 1 : null )
+      .catch(e => console.log('error: ', e))
+  }
+
+  return Promise.all([callPreviousPage, callNextPage])
+    .then(res => pages)
+    .catch(e => console.log('error: ', e))
+
 };
 
-// getPageNeighborsHelper Helper
-const getPageNeighborsHelper = (path, options) => {
+const getPageNeighborsHelper = (path, options={}) => {
 
-  if (options === undefined) {
-    options = {};
-  };
-  // Call queryString and queryStringHelper
-  let queryString = queryStringHelper(options, path).toString();
+  // call buildQueryString
+  let queryString = buildQueryString(options, path).toString();
 
-  return fetch(queryString).then(res => res.json()).then(res => res).catch(e => console.log("Error: ", e))
-}
+  return fetch(queryString)
+    .then(res => res.json())
+    .then(res => res)
+    .catch(e => console.log('error: ', e))
+};
 
 export default retrieve;
